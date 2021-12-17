ef9baa8c3d96725f62b826061a0b51a8ddd2d9ec
Alexander Korniichuk <world.kor.films@gmail.com>
2021-12-17 17:02:47 +0200

    maskify

--- src/components/MyApp/MyApp.js
+++ src/components/MyApp/MyApp.js
@@ -51,7 +51,7 @@ export const MyApp = () => {
             .map(join(" "))
 
 
-    console.log(my_alphabetPosition("The sunset sets at twelve o' clock.").orElse(console.error));
+    // console.log(my_alphabetPosition("The sunset sets at twelve o' clock.").orElse(console.error));
 
     // const arr = [2, 4, 5, 7, 8, 9, 10]
     // const str = "The sunset sets at twelve o' clock.";
@@ -99,7 +99,29 @@ export const MyApp = () => {
             .join("")
 
 
-    console.log(toCamelCase("the-stealth-warrior"));
+    // console.log(toCamelCase("the-stealth-warrior"));
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//     Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+//
+//         Your task is to write a function maskify, which changes all but the last four characters into '#'.
+//
+//         Examples
+//     maskify("4556364607935616") == "############5616"
+//     maskify(     "64607935616") ==      "#######5616"
+//     maskify(               "1") ==                "1"
+//     maskify(                "") ==                 ""
+//
+// // "What was the name of your first pet?"
+//     maskify("Skippy")                                   == "##ippy"
+//     maskify("Nananananananananananananananana Batman!") == "####################################man!"
+
+    const maskify = (cc) =>
+        cc.slice(0, -4)
+            .replace(/./g, '#') + cc.slice(-4);
+
+
+    console.log(maskify("4556364607935616"));
 
 
     return (
