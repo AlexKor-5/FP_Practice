b035e1f864abb9056a68fbf1385731e9cf3c05a3
Alexander Korniichuk <world.kor.films@gmail.com>
2021-12-21 17:30:12 +0200

    Updated! TypeError: Cannot set property 'innerHTML' of null

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import "./index.css";
 import {MyApp} from "./components/MyApp/MyApp";
-import {MySecondApp} from "./components/MySecondApp/MySecondApp";
+import {MySecondApp} from "./components/MySecondApp/MySecondApp.jsx";
 
 const destination = document.querySelector("#container");
 
