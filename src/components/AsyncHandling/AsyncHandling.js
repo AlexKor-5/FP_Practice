283e6cc3ce22710d3ea4269cc88b3914caadc4ed
Alexander Korniichuk <world.kor.films@gmail.com>
2021-12-29 10:03:06 +0200

    Set of parallel requests (src/components/AsyncHandling)

--- src/components/AsyncHandling/AsyncHandling.js
+++ src/components/AsyncHandling/AsyncHandling.js
@@ -16,6 +16,7 @@ import {
     curry,
     props,
     join,
+    split
 } from "ramda"
 import IO from "../../monads/IO/IO"
 
@@ -27,47 +28,40 @@ const append = curry((elementId, info) => {
     return info;
 });
 
-const result = getJSON('https://61cad4fd194ffe0017788980.mockapi.io/students')
-    .then(json => json)
-    .then(filter(s => s.address.country === 'US'))
-    .then(sortBy(prop('ssn')))
-    .then(tap(data => console.log(data)))
-    .then(map(student => {
-        return getJSON(`https://61cad4fd194ffe0017788980.mockapi.io/students/${student.id}/grades`)
-            .then(pluck('grades'))
-            .then(head)
-            .then(compose(Math.ceil, fork(divide, sum, length)))
-            .then(grade => {
-                return IO.of(mergeRight(student, {grade: grade}))
-                    .map(props(["name", "ssn", "grade"]))
-                    .map(join(","))
-                    .map(console.log).run()
-            })
-            .catch(e => console.error(e))
-    }))
-    .catch(e => console.error(e))
-
-
-// getJSON('/students')
-//     .then(hide('spinner'))
-//     .then(R.filter(s => s.address.country == 'US'))
-//     .then(R.sortBy(R.prop('ssn')))
-//     .then(R.map(student => {
-//         return getJSON('/grades?ssn=' + student.ssn)
-//             .then(R.compose(Math.ceil,
-//                 forkJoin(R.divide, R.sum, R.length)))
-//             .then(grade =>
-//                 IO.of(R.merge(student,
-//                     {'grade': grade}))
-//                     .map(R.props(['ssn', 'firstname',
-//                         'lastname', 'grade']))
-//                     .map(csv)
-//                     .map(append('#student-info')).run())
-//     );
+// const result = getJSON('https://61cad4fd194ffe0017788980.mockapi.io/students')
+//     .then(json => json)
+//     .then(filter(s => s.address.country === 'US'))
+//     .then(sortBy(prop('ssn')))
+//     .then(tap(data => console.log(data)))
+//     .then(map(student => {
+//         return getJSON(`https://61cad4fd194ffe0017788980.mockapi.io/students/${student.id}/grades`)
+//             .then(pluck('grades'))
+//             .then(head)
+//             .then(compose(Math.ceil, fork(divide, sum, length)))
+//             .then(grade => {
+//                 return IO.of(mergeRight(student, {grade: grade}))
+//                     .map(props(["name", "ssn", "grade"]))
+//                     .map(join(","))
+//                     .map(console.log).run()
+//             })
+//             .catch(e => console.error(e))
 //     }))
-//     .catch(function(error) {
-//         console.log('Error occurred: ' + error.message);
-//     });
+//     .catch(e => console.error(e))
+
+const average = compose(Math.ceil, fork(divide, sum, length))
+
+const result2 = getJSON('https://61cad4fd194ffe0017788980.mockapi.io/students')
+    .then(map(student => `https://61cad4fd194ffe0017788980.mockapi.io/students/${student.id}/grades`))
+    .then(gradeUrls => Promise.all(map(getJSON, gradeUrls)))
+    .then(map(item => head(item)))
+    .then(map(obj => prop('grades', obj)))
+    .then(map(arr => join(",", arr)))
+    .then(join(","))
+    .then(split(","))
+    .then(map(str => +str))
+    .then(average)
+    .then(grade => IO.of(grade).map(console.log).run())
+    .catch(e => console.error(e))
 
 
 export const AsyncHandling = () => {
